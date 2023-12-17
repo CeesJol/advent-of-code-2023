e1447017b14171688f15f6cb64ec89dc380a3fe0
Cees Jol <cjol99@icloud.com>
2023-12-17 13:28:31 +0100

    Add day9

---
+++ day9/task2.py
@@ -0,0 +1,36 @@
+from puzzle_input import example_input, puzzle_input
+
+CONFIG = 1 # 0 = example input, 1 = puzzle input
+INPUT = example_input if CONFIG == 0 else puzzle_input
+
+su = 0
+
+# Parse input
+for line_idx, line in enumerate(INPUT.splitlines()):
+    if len(line) == 0:
+        continue
+    numbers = [int(n) for n in line.split(" ")]
+
+    # Collect sequences: difference between each number
+    sequences = [numbers]
+    while True:
+        cur_sequence = sequences[-1]
+        new_sequence = []
+        for i in range(len(cur_sequence) - 1):
+            new_sequence.append(cur_sequence[i + 1] - cur_sequence[i])
+        # Check if new_sequence is zeroes only
+        sequences.append(new_sequence)
+        if all([n == 0 for n in new_sequence]):
+            break
+        
+    # Prepend 0 to each sequence
+    for sequence in sequences:
+        sequence.insert(0, 0)
+    # Backwards calculate new numbers
+    for s_idx in range(len(sequences) - 2, -1, -1):
+        sequences[s_idx][0] = sequences[s_idx][1] - sequences[s_idx + 1][0]
+
+    print(sequences)
+    su += sequences[0][0]
+
+print("su", su)
